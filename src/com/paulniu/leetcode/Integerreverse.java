9699420dfc05540b12447fb2819c54ab7555d925
niupuyue <niupuyue@aliyun.com>
2021-09-07 20:47:46 +0800

    添加 整数翻转

---
+++ src/com/paulniu/leetcode/Integerreverse.java
@@ -0,0 +1,31 @@
+package com.paulniu.leetcode;
+
+/**
+ * 整数翻转
+ * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ * 如果反转后整数超过 32 位的有符号整数的范围[−231, 231− 1] ，就返回 0。
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ * <p>
+ * 示例1：
+ * 输入：x = 123
+ * 输出：321
+ */
+public class Integerreverse {
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+    }
+
+    public static int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            if (result < Integer.MIN_VALUE / 10 || result > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            int offset = x % 10;
+            x = x / 10;
+            result = result * 10 + offset;
+        }
+        return result;
+    }
+
+}
